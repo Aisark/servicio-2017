7b4d4bb722d00975cd0b2bbab26fe71a63554ac1
DarkSylar117 <t.anto.j@gmail.com>
2017-06-27 15:03:28 -0500

    Change: callbacks por promise, ponery obtener datos a objetos item,

--- modulos/config.js
+++ modulos/config.js
@@ -1,24 +1,44 @@
 const storage = require('electron-storage');
 const jstorage = require('electron-json-storage');
 const path = require('path')
-const fs = require('fs');
+const fs = require('fs')
 
 var object;
 
+var setObject= function (callback) {
 
-exports.getSettings = function (callback) {
-  jstorage.get('settings',function (error,data) {
-    if (error){
-      callback(error);
-    }
-    callback(0,data)
-  })
 }
-
-exports.setSettings = function (valor,callback){
-  jstorage.set('settings', { volEffects: valor }, function(error) {
-    if (error){
-      callback(error)
-    };
+exports.getOBjc = function () {
+  return new Promise(function(resolve, reject) {
+    jstorage.get('settings',function (error,data) {
+      if(error){
+        reject(error);
+      }
+      object = data;
+      resolve(data)
+    })
   });
+}
+exports.setObject = function () {
+  return new Promise(function(resolve, reject) {
+    jstorage.set('settings',object,function (error) {
+      if (error) reject(erro);
+      resolve()
+    })
+  });
+}
+exports.getItemObj = function (item,item2) {
+  if(item2===undefined){
+    return object[item]
+  }else{
+    return object[item][item2]
+  }
+
+}
+exports.setItemObject = function (data,item,item2) {
+  if(item2===undefined){
+    object[item] = data
+  }else{
+    object[item][item2] = data
+  }
 }
