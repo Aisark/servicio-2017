30bbf30cc7da50ff896fd80da32d6a9b77d70017
Aisark <t.anto.j@gmail.com>
2017-07-14 19:08:21 -0500

    Change: Javascript to JQuery

--- js/renderProcess/render.js
+++ js/renderProcess/render.js
@@ -1,26 +1,31 @@
 const remote = require('electron').remote
 
-const closeBrows = document.getElementById('closeBrows')
-const recargar = document.getElementById('reload')
-const mini = document.getElementById('minimizar')
-
 //Cierra la ventana  actual
 function cerrar() {
-  var win = remote.getCurrentWindow()
-  win.close()
+    var win = remote.getCurrentWindow()
+    win.close()
 }
 //Recarga la ventana actual
 function reload() {
-  var win = remote.getCurrentWindow()
-  win.reload()
+    var win = remote.getCurrentWindow()
+    win.reload()
 }
 //Minimizar la ventana actual
 function minimizar() {
-  var win = remote.getCurrentWindow()
-  win.minimize();
+    var win = remote.getCurrentWindow()
+    win.minimize();
 }
 
-closeBrows.onclick = cerrar
-recargar.onclick = reload
-mini.onclick = minimizar
+$('#closeBrows').click(function() {
+    cerrar()
+})
+
+$('#reload').click(function() {
+    reload()
+})
+
+$('#minimizar').click(function() {
+    minimizar()
+})
+
 //----------------------
