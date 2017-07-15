1eab61e0ba16f904dc710de60e3e4d550e8da2ad
Aisark <t.anto.j@gmail.com>
2017-07-14 19:09:35 -0500

    Add: Guardar datos en settings.json

--- js/ScriptsDB/login.js
+++ js/ScriptsDB/login.js
@@ -1,66 +1,63 @@
 const db = require('./basedatos.js')
-const {ipcRenderer} = require('electron')
+const { ipcRenderer } = require('electron')
+const config = require('../../modulos/config.js')
 
-function logUSer(user,tipo) {
-  var pasw = $('#pass'+user).val()
-  var user = $('#user'+user).val()
+function logUSer(user, tipo) {
+    var pasw = $('#pass' + user).val()
+    var user = $('#user' + user).val()
 
-  if(!user==''&&!pasw==''){
-    db.getUser(user,tipo).then(function (data) {
-      if(data.PWORD===pasw){
-        $('.habilitado').addClass('desabilitado')
-        $('.progress').removeClass('hide')
-        ipcRenderer.send('show-window',0)
-        Materialize.toast('Bienvenido '+user, 4000)
-      }else{
-        Materialize.toast('Contraseña Incorrecta', 4000)
-      }
-    })
-    .catch(function () {
-      Materialize.toast('El usuario no esta registrado', 4000)
-    })
-  }else{
-    Materialize.toast('Datos vacios', 4000)
-  }
+    if (!user == '' && !pasw == '') {
+        db.getUser(user, tipo).then(function(data) {
+            if (data.PWORD === pasw) {
+                $('.habilitado').addClass('desabilitado')
+                $('.progress').removeClass('hide')
+                setConfig(user, data)
+                db.close()
+                ipcRenderer.send('show-window', 0)
+                Materialize.toast('Bienvenido ' + user, 4000)
+            } else {
+                Materialize.toast('Contraseña Incorrecta', 4000)
+            }
+        }).catch(function() {
+            Materialize.toast('El usuario no esta registrado', 4000)
+        })
+    } else {
+        Materialize.toast('Datos vacios', 4000)
+    }
 
+}
+
+
+function setConfig(user, data) {
+    config.setItemObject(user, 'userData', 'userName')
+    config.setItemObject(data.NOMBRE, 'userData', 'name')
+    config.setItemObject(data.AP_PAT, 'userData', 'lastName')
+    config.setObject()
 }
 
 function setTeachList() {
-  try {
-    db.getMaestros().then(function (array) {
-      array.forEach(function (data) {
-        var elem = '<option value="'+data.rowid+'">'+data.NOMBRE+' '+data.AP_PAT+'</option>'
+    db.getMaestros().then(function(array) {
+        array.forEach(function(data) {
+            var elem = '<option value="' + data.rowid + '">' + data.NOMBRE + ' ' + data.AP_PAT + '</option>'
+            $('select').append(elem)
+        })
+        $('select').material_select();
+    }).catch(function(err) {
+        var elem = '<option>' + ' No hay maestros ' + '</option>'
         $('select').append(elem)
-      })
-      $('select').material_select();
-    }).catch(function (err) {
-      var elem = '<option>'+' No hay maestros '+'</option>'
-      $('select').append(elem)
-      Materialize.toast(err, 10000)
+        Materialize.toast(err, 10000)
     })
-  } catch (e) {
-    Materialize.toast(e+' set',20000)
-  }
-
 }
 
-$('#senStuden').on('click',function () {
-  try {
-    logUSer('Stu','ALUMNOS')
-  } catch (e) {
-    Materialize.toast(e,20000)
-  }
+$('#senStuden').on('click', function() {
+    logUSer('Stu', 'ALUMNOS')
 })
 
-$('#sendMa').on('click',function () {
-  logUSer('Teach','MAESTROS')
+$('#sendMa').on('click', function() {
+    logUSer('Teach', 'MAESTROS')
 })
 
-$(document).ready(function () {
-  try {
+$(document).ready(function() {
     setTeachList()
-  } catch (e) {
-    Materialize.toast(e,20000)
-  }
-
+    config.getOBjc().then(function() {})
 })
--- js/ScriptsDB/signUp.js
+++ js/ScriptsDB/signUp.js
@@ -1,117 +1,121 @@
 const db = require('./basedatos.js')
-const {ipcRenderer} = require('electron')
+const { ipcRenderer } = require('electron')
+const config = require('../../modulos/config.js')
 
-var datos = ['','','','','','H',0] //['nombre','apP','apM','user','pass','sexo',0]
+var datos = ['', '', '', '', '', 'H', 0] //['nombre','apP','apM','user','pass','sexo',0]
 
-function getDataUser(item,index) {
-  $('#'+item+'Us').change(function () {
-    var valor = $(this).val()
-    if(valor!==''&&valor.length>2){
-      datos[index] = valor
-    }else{
-      datos[index] = ''
-    }
-  })
+function setConf(data) {
+    config.setItemObject(data[3], 'userData', 'userName')
+    config.setItemObject(data[0], 'userData', 'name')
+    config.setItemObject(data[1], 'userData', 'lastName')
+    config.setObject()
+}
+
+function getDataUser(item, index) {
+    $('#' + item + 'Us').change(function() {
+        var valor = $(this).val()
+        if (valor !== '' && valor.length > 2) {
+            datos[index] = valor
+        } else {
+            datos[index] = ''
+        }
+    })
 }
 
 function checkUserAvi() {
-  $('#nickUs').change(function () {
-    var tipo = $('input:radio[name=tipoUser]:checked').val()
-    var dato = $(this).val()
-    console.log(dato);
-    if(dato.length>3){
-      db.getUserAvia(tipo).then(function (array) {
-        if(array.length>0){
-          array.forEach(function (ele) {
-            if(dato===ele.USERNAME){
-              datos[3]=''
-              Materialize.toast(dato+' no esta disponible', 4000)
-            }
-            else{
-              datos[3] = dato
-            }
-          })
-        }else{
-          datos[3] = dato
+    $('#nickUs').change(function() {
+        var tipo = $('input:radio[name=tipoUser]:checked').val()
+        var dato = $(this).val()
+        console.log(dato);
+        if (dato.length > 3) {
+            db.getUserAvia(tipo).then(function(array) {
+                if (array.length > 0) {
+                    array.forEach(function(ele) {
+                        if (dato === ele.USERNAME) {
+                            datos[3] = ''
+                            Materialize.toast(dato + ' no esta disponible', 4000)
+                        } else {
+                            datos[3] = dato
+                        }
+                    })
+                } else {
+                    datos[3] = dato
+                }
+            })
+        } else {
+            Materialize.toast('El nombre de usuario debe contener minimo 4 letras', 4000)
+            datos[3] = ''
         }
-      })
-    }else{
-      Materialize.toast('El nombre de usuario debe contener minimo 4 letras', 4000)
-      datos[3] = ''
-    }
-  })
+    })
 }
 
 function confirPass() {
-  $('#cpassUs').change(function () {
-    var pass = $('#passUs').val()
-    var cpass = $(this).val()
-    if(pass===cpass){
-      datos[4] = pass
-    }else {
-      Materialize.toast('La contraseña no coincide', 4000)
-      datos[4] = ''
-    }
-  })
+    $('#cpassUs').change(function() {
+        var pass = $('#passUs').val()
+        var cpass = $(this).val()
+        if (pass === cpass) {
+            datos[4] = pass
+        } else {
+            Materialize.toast('La contraseña no coincide', 4000)
+            datos[4] = ''
+        }
+    })
 }
 
 function getSex() {
-  $('input:radio[name=sexo]').click(function () {
-    datos[5] = $(this).val()
-    console.log(datos[5]);
-  })
+    $('input:radio[name=sexo]').click(function() {
+        datos[5] = $(this).val()
+        console.log(datos[5]);
+    })
 }
 
 function getIDMaestro() {
-  $('#selMaes').change(function () {
-    var id = parseInt($(this).val())
-    datos[6] = id
-  })
+    $('#selMaes').change(function() {
+        var id = parseInt($(this).val())
+        datos[6] = id
+    })
 }
 
 function sendSignUp() {
-  $('#sendSign').click(function () {
-    var tipo = $('input:radio[name=tipoUser]:checked').val()
-    var flag = true
-    for (var i = 0; i < datos.length; i++) {
-      if(datos[i]===''||datos[i]===0){
-        flag = false
-        break
-      }
-    }
-    if (flag) {
-      Materialize.toast('Datos correctos', 4000)
-      if(tipo==='ALUMNOS'){
-        db.setStudents(datos).then(function () {
-          $('.habilitado').addClass('desabilitado')
-          $('.progress').removeClass('hide')
-          Materialize.toast('Usuario registrado', 4000)
-          ipcRenderer.send('show-window',0)
-        })
-      }else{
-        Materialize.toast('Sección de maestros en construcción', 4000)
-      }
-    }
-    else{
-      console.log(datos);
-      Materialize.toast('Hay datos vacios', 4000)
-    }
-  })
+    $('#sendSign').click(function() {
+        var tipo = $('input:radio[name=tipoUser]:checked').val()
+        var flag = true
+        for (var i = 0; i < datos.length; i++) {
+            if (datos[i] === '' || datos[i] === 0) {
+                flag = false
+                break
+            }
+        }
+        if (flag) {
+            Materialize.toast('Datos correctos', 4000)
+            if (tipo === 'ALUMNOS') {
+                db.setStudents(datos).then(function() {
+                    $('.habilitado').addClass('desabilitado')
+                    $('.progress').removeClass('hide')
+                    Materialize.toast('Usuario registrado', 4000)
+                    db.close()
+                    ipcRenderer.send('show-window', 0)
+                })
+                setConf(datos)
+            } else {
+                Materialize.toast('Sección de maestros en construcción', 4000)
+            }
+        } else {
+            console.log(datos);
+            Materialize.toast('Hay datos vacios', 4000)
+        }
+    })
 }
 
-$(document).ready(function () {
-  try {
-    getDataUser('name',0)
-    getDataUser('app',1)
-    getDataUser('apm',2)
+$(document).ready(function() {
+    getDataUser('name', 0)
+    getDataUser('app', 1)
+    getDataUser('apm', 2)
     checkUserAvi()
     confirPass()
     getSex()
     getIDMaestro()
     sendSignUp()
-  } catch (e) {
-    Materialize.toast(e,50000)
-  }
 })
 
 
