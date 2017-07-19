8b81f1b84d62e90b9d374735f0d0ef1107a2daa2
Aisark <t.anto.j@gmail.com>
2017-07-19 13:20:19 -0500

    Add: Nombre del usuario

--- js/Events/init.js
+++ js/Events/init.js
@@ -1,13 +1,10 @@
 const btn = require('../../js/Events/btn.js');
 const proceso = require('../../js/Equations/procSolucion');
 const pasos = require('../../js/Equations/pasos.js');
+const config = require('../../modulos/config.js')
+const str = require('../../modulos/format.js');
 
-$(document).ready(function() {
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    var _closeV = document.getElementById('closeTM')
-    var _video = document.getElementById('vTutorial')
-
-
+function modals() {
     $('#tipsM').modal({
         complete: function() { _video.pause() },
         ready: function() {
@@ -15,14 +12,31 @@ $(document).ready(function() {
                 _video.play()
             }, 1000);
         }
-    });
-
+    })
     $('#solucionM').modal({
         ready: function() { proceso.procSol() },
         complete: function() {
             btn.updateEcua()
             pasos.resetSteps();
         }
-    });
+    })
+}
 
+function loadObj() {
+    config.getOBjc().then(function() {
+        loadUser()
+    })
+}
+
+function loadUser() {
+    var obj = config.getItemObj('userData')
+    var nombre = str.camelUp(obj['NOMBRE']) + str.camelUp(obj['AP_PAT']) + str.camelUp(obj['AP_MAT'])
+    $('#nombre').text(nombre)
+}
+
+$(document).ready(function() {
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    var _video = document.getElementById('vTutorial')
+    modals()
+    loadObj()
 });
