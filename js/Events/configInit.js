2fb36d776967e80f77e106204139d6beb9198b47
Aisark <t.anto.j@gmail.com>
2017-07-19 13:19:01 -0500

    Add: Modificar datos del perfil de usuario

--- js/Events/configInit.js
+++ js/Events/configInit.js
@@ -1,8 +1,15 @@
 const noUiSlider = require('../../noUiSlider/nouislider.min.js')
 const wNumb = require('../../noUiSlider/wNumb.js')
 const config = require('../../modulos/config.js')
+const db = require('../../js/ScriptsDB/basedatos.js')
 
 var slider = document.getElementById('volRange')
+
+var usData = {}
+var newpw = "",
+    chpw = false,
+    usuario = "",
+    chuser = false
 
 /**
  * @summary Creat object noUislider
@@ -37,14 +44,130 @@ noUiSlider.create(slider, {
  **/
 function loadObject() {
     config.getOBjc().then(function() {
-        var nombre = config.getItemObj('userData', 'name') + ' ' + config.getItemObj('userData', 'lastName')
+        usData = Object.assign(config.getItemObj('userData'))
+        var nombre = usData.NOMBRE + ' ' + usData.AP_PAT
         $('#banner').text('BIENVENIDO ' + nombre)
+        setDataConfi()
     })
 }
 
-$(document).ready(function() {
-    loadObject()
-})
+function iteDataobj() {
+    for (var key in usData) {
+        if (usData.hasOwnProperty(key)) {
+            $('#' + key).val(usData[key])
+        }
+    }
+}
+
+function setDataConfi() {
+    iteDataobj()
+    Materialize.updateTextFields()
+}
+
+exports.saveNewData = function() {
+    return new Promise(function(resolve, reject) {
+        var pass = $('#PWORD').val()
+        if (pass !== "") {
+            db.isPassCorrect(pass, usData.USERNAME, 'ALUMNOS').then(function(bol) {
+                var object = {}
+                var inputs = $('.updb')
+                if (bol) {
+                    for (var key in inputs) {
+                        if (inputs.hasOwnProperty(key)) {
+                            var input = inputs[key]
+                            var id = input.id
+                            if (input.localName == 'input') {
+                                if (input.value != usData[id] && id != 'USERNAME') {
+                                    object[id] = input.value
+                                }
+                            }
+                        }
+                    }
+                    if (chpw) {
+                        object['PWORD'] = newpw
+                    }
+                    if (chuser) {
+                        object['USERNAME'] = usuario
+                    }
+                    if (Object.keys(object).length != 0) {
+                        var user = usData['USERNAME']
+                        for (var key in object) {
+                            if (object.hasOwnProperty(key) && key != 'PWORD') {
+                                usData[key] = object
+                            }
+                        }
+                        config.setItemObject(usData, 'userData')
+                        db.updatePerf(object, 'ALUMNOS', user).catch(function(error) {
+                            reject('No se actualizaron los datos')
+                        })
+                    }
+                    restInpuPw()
+                    resolve()
+                } else {
+                    reject('Contraseña incorrecta')
+                }
+            })
+        } else {
+            reject('Ingresa la contraseña')
+        }
+    });
+
+
+}
+
+exports.cancelData = function() {
+    iteDataobj()
+    restInpuPw()
+    Materialize.updateTextFields()
+}
+
+function confirPass() {
+    $('#PWORDCN').change(function() {
+        var pass = $('#PWORDN').val()
+        var cpass = $(this).val()
+        if (pass !== cpass) {
+            chpw = false
+            Materialize.toast('La contraseña no coincide', 4000)
+        } else {
+            if (pass !== undefined && cpass !== undefined) {
+                newpw = pass
+                chpw = true
+            } else {
+                chpw = false
+            }
+
+        }
+    })
+}
+
+function restInpuPw() {
+    $('#PWORD').val('')
+    $('#PWORDCN').val('')
+    $('#PWORDN').val('')
+}
+
+function checkUser() {
+    $('#USERNAME').change(function() {
+        usuario = $(this).val()
+        if (usuario.length > 3) {
+            db.getUserAvia('ALUMNOS').then(function(array) {
+                for (var i = 0; i < array.length; i++) {
+                    if (array[i].USERNAME === usuario) {
+                        chuser = false
+                        Materialize.toast('Nombre de usuario no valido', 4000)
+                        break
+                    } else {
+                        chuser = true
+                    }
+                }
+            })
+        } else {
+            chuser = false
+            Materialize.toast('Nombre de usuario no valido ' + chuser, 4000)
+        }
+    })
+}
+
 
 /**
  * @summary Actualizar configuraciones
@@ -84,15 +207,19 @@ $('#agreGame').on('click', function() {
         url = document.getElementById('urlimg').files,
         urlImg = (url.length !== 0) ? url[0].path : ""
 
-
-
     config.setItemObject(nPuzz, 'lvlPuzz')
     config.setItemObject(nEcua, 'lvlEcua')
     config.setItemObject(urlImg, 'urlImg')
 
-    config.setObject().catch(function(error) {
-        console.log(error);
-    })
+    config.setObject().catch(function(error) {})
 })
+
+
+$(document).ready(function() {
+    loadObject()
+    confirPass()
+    checkUser()
+})
+
 
 //
