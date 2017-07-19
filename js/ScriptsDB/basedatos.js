0d9460a69217c610d22fc821cd392b4aeb2611a4
Aisark <t.anto.j@gmail.com>
2017-07-19 13:21:09 -0500

    Add: Funcion actualizar table, verificacion de contraseña

--- js/ScriptsDB/basedatos.js
+++ js/ScriptsDB/basedatos.js
@@ -3,6 +3,7 @@ const util = require('util');
 
 const db = new sqlite3.Database('Users.db');
 
+
 const tabMaes = `CREATE TABLE MAESTROS(
                 NOMBRE VARCHAR(30),
                 AP_PAT VARCHAR(10),
@@ -11,15 +12,29 @@ const tabMaes = `CREATE TABLE MAESTROS(
                 USERNAME VARCHAR(8) UNIQUE)`
 const insMaes = `insert into MAESTROS values("OBI","WAN","KENOBI","cooksith","benkeno")`
 const tabAlum = `CREATE TABLE ALUMNOS(
-                 NUMCON INT UNIQUE,NOMBRE VARCHAR(30),AP_PAT VARCHAR(10),AP_MAT VARCHAR(10),
-                 PWORD VARCHAR(15),USERNAME VARCHAR(8) UNIQUE,SEXO VARCHAR(1),ID_MAESTRO INTEGER,
-                 MAX_LVL_ECU VARCHAR(3),ECU_RESOL INT,ECU_FAIL INT,TIME_GAME INT,CHECK (SEXO IN ("H","M")),
-                 CHECK (MAX_LVL_ECU IN ("FAC","MED","DIF")),FOREIGN KEY (ID_MAESTRO) REFERENCES MAESTROS(rowid))`
+                    NUMCON INT UNIQUE,
+                    NOMBRE VARCHAR(30),
+                    AP_PAT VARCHAR(10),
+                    AP_MAT VARCHAR(10),
+                    PWORD VARCHAR(15),
+                    USERNAME VARCHAR(8) UNIQUE,
+                    SEXO VARCHAR(1),
+                    ID_MAESTRO INTEGER,
+                    SEMESTRE INTEGER,
+                    GRUPO VARCHAR(1),
+                    MAX_LVL_ECU VARCHAR(3),
+                    ECU_RESOL INT,
+                    ECU_FAIL INT,
+                    TIME_GAME INT,
+                    CHECK (SEXO IN ('H','h','m','M')),
+                    CHECK (MAX_LVL_ECU IN ('FAC','MED','DIF')),
+                    FOREIGN KEY (ID_MAESTRO) REFERENCES MAESTROS(rowid)
+                )`
 
 exports.getUser = function(user, tipo) {
     return new Promise(function(resolve, reject) {
         db.parallelize(function() {
-            var query = util.format("select PWORD,NOMBRE,AP_PAT from %s where USERNAME='%s'", tipo, user)
+            var query = util.format("select PWORD,NOMBRE,AP_PAT,AP_MAT from %s where USERNAME='%s'", tipo, user)
             db.get(query, function(err, data) {
                 if (err || data == undefined) reject(err)
                 resolve(data)
@@ -28,6 +43,16 @@ exports.getUser = function(user, tipo) {
     });
 }
 
+exports.isPassCorrect = function(pass, user, tipo) {
+    return new Promise(function(resolve, reject) {
+        var query = util.format("select PWORD from %s where USERNAME='%s'", tipo, user)
+        db.serialize(function() {
+            db.each(query, function(err, row) {
+                resolve(pass === row.PWORD)
+            })
+        })
+    });
+}
 exports.getMaestros = function() {
     return new Promise(function(resolve, reject) {
         db.parallelize(function() {
@@ -84,6 +109,34 @@ exports.checkedTables = function() {
             })
         })
     });
+}
+
+exports.updatePerf = function(object, tipo, user) {
+    return new Promise(function(resolve, reject) {
+        var array = Object.keys(object),
+            query = "update " + tipo + " set ",
+            subquery = "",
+            whquery = " where USERNAME=" + "'" + user + "'"
+        console.log('database ' + tipo + object);
+        array.forEach(function(ele, index) {
+            if (index !== (array.length - 1)) {
+                var str = ele + "=" + "'" + object[ele] + "'" + ","
+                subquery += str
+            } else {
+                var str = ele + "=" + "'" + object[ele] + "'"
+                subquery += str
+            }
+        });
+        console.log(query + subquery + whquery);
+        db.parallelize(function() {
+            try {
+                db.run(query + subquery + whquery)
+                resolve()
+            } catch (e) {
+                reject(e)
+            }
+        })
+    })
 }
 
 exports.close = function() {
