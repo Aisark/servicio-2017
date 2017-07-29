bd36e5090847e4292ed8554ad089924a501313cb
Aisark <t.anto.j@gmail.com>
2017-07-29 15:05:59 -0500

    Change: Nombres de atributos del objeto _terminos

--- js/Equations/resolucion.js
+++ js/Equations/resolucion.js
@@ -1,31 +1,33 @@
 const ecuacion = require('../../js/Equations/funciones.js');
+const solu = require('../../js/Equations/solucion.js');
 
 var _terminos = {
     errors: 0,
     correct: 0,
     global: 0,
     score: 0,
-    m1X: 0,
-    m1I: 0,
-    m2X: 0,
-    x: 0,
-    c: 0
+    lvl: "",
+    ecuacion: [],
+    m: ["", ""],
+    mp: "",
+    mp2: "",
+    mp3: "",
+    res: [0, 0]
 }
 
 //Suma la cantidad de terminos independientes y dependientes de ambos mienbros de una
 //funcion
 function respuesta() {
-    var ax1 = _terminos.m1.split(' ')
-    var ax2 = _terminos.m2.split(' ')
+    var ax1 = _terminos.m[0].split(' ')
+    var ax2 = _terminos.m[1].split(' ')
 
     var a1 = splitEcua(ax1)
     var a2 = splitEcua(ax2)
-    _terminos.m1X = a1[0]
-    _terminos.m1I = a1[1]
-    _terminos.m2X = a2[0]
-    _terminos.m2I = a2[1]
-    _terminos.x = (a1[0] - a2[0])
-    _terminos.c = (a2[1] - a1[1])
+    _terminos.mp = a1[0] + "x " + a1[1] + " = " + a2[0] + "x " + a2[1]
+    _terminos.mp2 = a1[0] + "x " + (-1 * a2[0]) + "x = " + a2[1] + " " + (-1 * a1[1])
+    _terminos.mp3 = "x = " + (a2[1] - a1[1]) + "/" + (-1 * (a1[0] - a2[0]))
+    _terminos.res[1] = (a1[0] - a2[0])
+    _terminos.res[0] = (a2[1] - a1[1])
         //console.log(_terminos.c + '/' + _terminos.x);
 }
 
@@ -59,13 +61,15 @@ function splitEcua(array) {
 
 exports.setEcuacion = function(lvl) {
     var n = ecuacion.ecuacion(lvl)
-    _terminos.m1 = (lvl == 1) ? n[0] : ecuacion.reduce(n[0])
-    _terminos.m2 = (lvl == 1) ? n[1] : ecuacion.reduce(n[1])
+    _terminos.m[0] = (lvl == 1) ? n[0] : ecuacion.reduce(n[0])
+    _terminos.m[1] = (lvl == 1) ? n[1] : ecuacion.reduce(n[1])
     respuesta()
     n[0] = (lvl == 1) ? n[0] : ecuacion.clsEcua(n[0])
     n[1] = (lvl == 1) ? n[1] : ecuacion.clsEcua(n[1])
+    _terminos.ecuacion = n
     var cadena = n[0] + ' ' + ' = ' + ' ' + n[1]
     $('#ecuaMath').text(cadena)
+    solu.getObj(lvl).then(function() {})
 }
 
 
