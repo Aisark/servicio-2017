0881c46975ce2b7bf2f9269028251511e757e5cf
Aisark <t.anto.j@gmail.com>
2017-07-31 16:37:10 -0500

    Remove swipeable

--- js/Events/initLog.js
+++ js/Events/initLog.js
@@ -1,26 +1,23 @@
+$(document).ready(function() {
+    $('ul.tabs').tabs()
+        //Mostrar tab Registro
+    $('.regUser').on('click', function() {
+        $('ul.tabs').tabs('select_tab', 'tabSingUp');
+    })
 
-$(document).ready(function () {
-  $('ul.tabs').tabs({
-    swipeable:true
-  })
-  //Mostrar tab Registro
-  $('.regUser').on('click',function () {
-    $('ul.tabs').tabs('select_tab', 'tabSingUp');
-  })
-
-  $('input:radio[name=tipoUser]').on('click',function () {
-    var tipo = $(this).val()
-    if(tipo=='ALUMNOS'){
-      $('input[name=sexo]').prop('disabled',false)
-      $('#selMaes').prop('disabled',false)
-      $('label[for=codConf]').text('Num. Control')
-      $('select').material_select();
-    }else{
-      $('input[name=sexo]').prop('disabled',true)
-      $('#selMaes').prop('disabled',true)
-      $('label[for=codConf]').text('Token')
-      $('select').material_select();
-    }
-  })
+    $('input:radio[name=tipoUser]').on('click', function() {
+        var tipo = $(this).val()
+        if (tipo == 'ALUMNOS') {
+            $('input[name=sexo]').prop('disabled', false)
+            $('#selMaes').prop('disabled', false)
+            $('label[for=codConf]').text('Num. Control')
+            $('select').material_select();
+        } else {
+            $('input[name=sexo]').prop('disabled', true)
+            $('#selMaes').prop('disabled', true)
+            $('label[for=codConf]').text('Token')
+            $('select').material_select();
+        }
+    })
 
 })
